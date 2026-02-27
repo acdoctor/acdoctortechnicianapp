35d74686b278294276ad650b8e21f0cbc1fbff60
jatinacdoctor0-star <jatinacdoctor0@gmail.com>
2026-02-27 15:45:44 +0530

    demo test

---
+++ temo.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const temo = () => {
+  return (
+    <View>
+      <Text>temo</Text>
+    </View>
+  );
+};
+
+export default temo;
+
+const styles = StyleSheet.create({});
